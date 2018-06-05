e26445f01713597b64064ef9c5ed85a4d34608e6
bozhi <bozhi@suneee.com>
2018-06-05 09:31:00 +0800

    test

---
+++ test/src/main/java/Test.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018, 上海象翌微链有限公司
+ * FileName: Test
+ * Author:   suneee
+ * Date:     2018/6/5 9:29
+ * Description: test
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈test〉
+ *
+ * @author suneee
+ * @create 2018/6/5
+ * @since 1.0.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
