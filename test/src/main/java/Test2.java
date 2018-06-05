b0e91c55a638676902fb948ac7cb75557a4e87e2
bozhi <bozhi@suneee.com>
2018-06-05 09:56:38 +0800

    chore: Preserve Test2.java during merges

---
+++ test/src/main/java/Test2.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, 上海象翌微链有限公司
+ * FileName: Test2
+ * Author:   suneee
+ * Date:     2018/6/5 9:52
+ * Description: test2
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package PACKAGE_NAME;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈test2〉
+ *
+ * @author suneee
+ * @create 2018/6/5
+ * @since 1.0.0
+ */
+public class Test2 {
+
+}
